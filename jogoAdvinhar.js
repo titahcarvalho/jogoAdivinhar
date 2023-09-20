0cb5ac08e8b6b96cb5e3acb897f83dd86e175f1f
Tita <116283812+titahcarvalho@users.noreply.github.com>
2023-09-20 11:18:13 -0300

    Add files via upload
    Resolvendo o desafio de acrescentar duas dicas com os operadores MAIOR > e MENOR < para encontrar o número aleatório.

---
+++ jogoAdvinhar.js
@@ -0,0 +1,16 @@
+alert("JOGO DE ADIVINHAR O NÚMERO");
+let numero = parseInt(Math.random() * 10)+1;
+let entrada = -1;
+while (entrada != numero ) {
+    entrada = prompt("ENTRE COM UM NÚMERO de 0 a 10.");
+    if (entrada != numero) { 
+        if( numero > entrada) {
+            alert("O número é MAIOR!");
+        } else if (numero < entrada)
+            {
+                alert ("O número é MENOR!");
+            }
+        alert ("TENTA NOVAMENTE!");
+                           }
+ }
+alert ("PARABÉNS! Você acertou!");
